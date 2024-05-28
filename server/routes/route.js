c059e487062e1c531fb2ed50c25f35c764266458
unknown <chaitanyalakra130@example.com>
2024-05-28 23:01:55 +0530

    order api

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { Usersignup , Userlogin } from '../controller/user-controller.js';
-import { GetProducts , GetProductsById } from '../controller/get-products.js';
+import { GetProducts , GetProductsById } from '../controller/get-products.js'; 
 
 
 const router = express.Router();
@@ -9,10 +9,15 @@ router.post('/signup' , Usersignup);
  
 router.post('/login' , Userlogin);
 
+router.post('/order' , Order);
+
 router.get('/products' , GetProducts);
 
 router.get('/products/:id' , GetProductsById);
 
+const Order =()=>{
+    return response.status(200).json("ok");
+}
 
 
 
